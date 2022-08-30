97073186009513e6464ba48fa67be37f9012c354
Carolina <cdimatteo@fi.uba.ar>
2022-08-29 23:54:54 -0300

    segundo ejercicio de if con ownership

---
+++ g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d34364c23203c642.d
@@ -0,0 +1,5 @@
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d34364c23203c642: src/main.rs
+
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d34364c23203c642.d: src/main.rs
+
+src/main.rs:
