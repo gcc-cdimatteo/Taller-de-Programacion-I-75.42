d4075d7e9d4d2d8de876d937764f9277d4fd7358
Carolina <cdimatteo@fi.uba.ar>
2022-09-01 11:39:00 -0300

    dependencias

---
+++ g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-c6ce0e5d39af3d87.d
@@ -0,0 +1,5 @@
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-c6ce0e5d39af3d87.rmeta: src/main.rs
+
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-c6ce0e5d39af3d87.d: src/main.rs
+
+src/main.rs:
---
+++ g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d57a0ecb12d80402.d
@@ -0,0 +1,5 @@
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d57a0ecb12d80402.rmeta: src/main.rs
+
+/home/carolina/Documentos/FIUBA/Taller de Programación I/g0_rustlings_if2/target/debug/deps/g0_rustlings_if2-d57a0ecb12d80402.d: src/main.rs
+
+src/main.rs:
