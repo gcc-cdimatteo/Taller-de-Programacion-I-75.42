91ecde1fc1204b0069a1169d7dd7dc88162569d1
Carolina <cdimatteo@fi.uba.ar>
2022-09-12 19:59:41 -0300

    agrego clase 04

---
+++ clase_04/src/00_server.rs
@@ -0,0 +1,42 @@
+//! Abre un puerto TCP en el puerto asignado por argv.
+//! Escribe las lineas recibidas a stdout y las manda mediante el socket.
+
+use std::env::args;
+use std::io::{BufRead, BufReader, Read};
+use std::net::{TcpListener, TcpStream};
+
+static SERVER_ARGS: usize = 2;
+
+fn main() -> Result<(), ()> {
+    let argv = args().collect::<Vec<String>>();
+    if argv.len() != SERVER_ARGS {
+        println!("Cantidad de argumentos inválido");
+        let app_name = &argv[0];
+        println!("{:?} <host> <puerto>", app_name);
+        return Err(());
+    }
+
+    let address = "0.0.0.0:".to_owned() + &argv[1];
+    server_run(&address).unwrap();
+    Ok(())
+}
+
+fn server_run(address: &str) -> std::io::Result<()> {
+    let listener = TcpListener::bind(address)?;
+    // accept devuelve una tupla (TcpStream, std::net::SocketAddr)
+    let connection = listener.accept()?;
+    let mut client_stream : TcpStream = connection.0;
+    // TcpStream implementa el trait Read, así que podemos trabajar como si fuera un archivo
+    handle_client(&mut client_stream)?;
+    Ok(())
+}
+
+fn handle_client(stream: &mut dyn Read) -> std::io::Result<()> {
+    let reader = BufReader::new(stream);
+    let mut lines = reader.lines();
+    // iteramos las lineas que recibimos de nuestro cliente
+    while let Some(line) = lines.next() {
+        println!("Recibido: {:?}", line);
+    }
+    Ok(())
+}
---
+++ clase_04/src/01_bin_protocol_cli.rs
@@ -0,0 +1,36 @@
+//! Se conecta mediante TCP a la dirección asignada por argv.
+//! Lee lineas desde stdin y las manda mediante el socket.
+mod alumno;
+
+use crate::alumno::Alumno;
+use std::env::args;
+use std::net::TcpStream;
+
+static CLIENT_ARGS: usize = 3;
+
+fn main() -> Result<(), ()> {
+    let argv = args().collect::<Vec<String>>();
+    if argv.len() != CLIENT_ARGS {
+        println!("Cantidad de argumentos inválido");
+        let app_name = &argv[0];
+        println!("{:?} <host> <puerto>", app_name);
+        return Err(());
+    }
+
+    let address = argv[1].clone() + ":" + &argv[2];
+    println!("Conectándome a {:?}", address);
+
+    client_run(&address).unwrap();
+    Ok(())
+}
+
+/// Client run recibe una dirección y cualquier cosa "legible"
+/// Esto nos da la libertad de pasarle stdin, un archivo, incluso otro socket
+fn client_run(address: &str) -> std::io::Result<()> {
+    // Vamos a mandar datos crudos
+    let mut socket = TcpStream::connect(address)?;
+    let alumno = Alumno {nombre: "Pepe Muleiro".to_owned(), padron: 80880};
+    println!("Enviando: {:?}", alumno);
+    alumno.write_to(&mut socket).unwrap();
+    Ok(())
+}
